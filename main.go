3325610e9614276e051c95653f1dea749d2227f7
Alexander Demchenko <alexandersd3306@gmail.com>
2025-05-27 23:22:14 +0500

    task

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import ( 
+	"fmt"
+)
+
+func main() {
+	cache := Cache.New()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+}
